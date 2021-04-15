fd4c883fe58d8362995360b90b1db3bbc609c5eb
wjl <2277963503@qq.com>
2021-04-15 20:51:55 +0800

    add 2 files: python and c type file

---
+++ c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int a) {
+	int sum_;
+	for (int i=0;i<=a;i++) {
+		sum_ = sum_ + i;
+	}
+	fprintf("sum from 0 to a is: %d",sum)
+	return 1;
+	
+}
---
+++ gala.py
@@ -0,0 +1,3 @@
+def gala(name):
+    print('your name is %s'%name)
+gala("wjl")
